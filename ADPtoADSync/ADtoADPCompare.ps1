84a2afd8ceb7f2c2f3b32e8a2961589ff2b19fff
tankcr <kristopher.roy@gmail.com>
2017-09-26 15:13:06 -0700

    Add
    added a script for comparing ADP and AD export files.

---
+++ ADPtoADSync/ADtoADPCompare.ps1
@@ -0,0 +1,35 @@
+﻿# ADPtoADPCompare.ps1
+# Created by Kristopher Roy
+# Script purpose - compare ADP output details back to AD prior to any writing changes
+$adpinput = import-csv C:\Belltech\ADPOutput_09_22_17.csv
+$ADusers = get-aduser -filter * -Properties userprincipalname,department,sn,MobilePhone,OfficePhone,City,postalCode,state,street,employeeID,mail,title,manager,description|select UserPrincipalName,givenName,sn,mail,department,MobilePhone,OfficePhone,City,postalCode,state,street,employeeID,title,manager,description,ADPName,NoADPMatch
+
+FOREACH($USER in $ADUSers)
+{
+    $adpuser = $adpinput|where{($_."Work Contact: Work Email").trim() -eq $user.mail}
+    If($adpuser -eq $null -or $adpuser -eq "")
+    {
+        #$adpuser = $adpinput|where{($_."employee_name").split(",")[0] -eq $user.sn}
+        #($adpuser.Employee_name.split(",")[1]).trim().split("")[0]
+        IF($User.sn -ne $null){
+        $adpmatch = $User.sn+", "+$User.givenName
+        $adpuser = $adpinput|where{$_.Employee_name -like "*"+$adpmatch+"*"}
+        #$adpln = (($adpinput|where{($_."employee_name").split(",")[0] -eq $user.sn})."employee_name").split(",")[0]
+        #$adpfn = (($adpinput|where{(($_."employee_name").split(",")[1]).split("")[0] -eq $user.givenName})."employee_name").split(",")[1]
+        #-and $_.DeptNumber+"*" -like $user.department}
+        #Write-Host $aduser "no match"
+        #$adpuser
+        $adpmatch = $null
+        }
+    }
+    If($adpuser -ne $null -and $adpuser -ne "")
+    {
+        $user.ADPName = $adpuser.Employee_name
+        $user.NoADPMatch = "False"
+        #$user.mail
+        #$adpuser.Employee_name
+    }
+    $adpuser = $null
+}
+
+$ADUSers|export-csv c:\belltech\ADP_AD_compare_09_26_17.csv
